57478bdcdf417607048fb9be7a89c756816027ee
Liam Tsang <lt1800@nyu.edu>
2024-02-27 18:30:34 -0500

    feat: figma auth v1

--- routes/auth.ts
+++ routes/auth.ts
@@ -1,57 +1,51 @@
 import { Context, Hono, Next } from "https://deno.land/x/hono@v4.0.3/mod.ts";
-
-import { load } from "https://deno.land/std/dotenv/mod.ts";
 import {
-  getRequiredEnv,
-  getSessionId,
-  handleCallback,
-  type OAuth2ClientConfig,
-  signIn,
-  signOut,
-} from "https://deno.land/x/deno_kv_oauth@v0.10.0/mod.ts";
+  deleteCookie,
+  getCookie,
+  getSignedCookie,
+  setCookie,
+  setSignedCookie,
+} from "https://deno.land/x/hono@v4.0.3/helper.ts";
 
-const configData = await load({
-  export: true,
-  allowEmptyValues: true,
-});
-
-const oauthConfig: OAuth2ClientConfig = {
-  clientId: getRequiredEnv("FIGMA_CLIENT_ID"),
-  clientSecret: getRequiredEnv("FIGMA_CLIENT_SECRET"),
-  authorizationEndpointUri: "https://www.figma.com/oauth",
-  tokenUri: "https://www.figma.com/api/oauth/token",
-  redirectUri: "https://design-battles.deno.dev/auth/oauth/callback",
-  defaults: {
-    scope: "files:read, file_comments:write, webhooks:write",
-  },
-};
+const clientId = Deno.env.get("FIGMA_CLIENT_ID") as string;
+const clientSecret = Deno.env.get("FIGMA_CLIENT_SECRET");
+const redirectUri = "http://localhost:8000/auth/oauth/callback";
+const scope = "files:read, file_comments:write, webhooks:write";
 
 const app = new Hono();
 
 app.get("/oauth/signin", async (c: Context) => {
-  return await signIn(c.req.raw, oauthConfig);
+  const state = crypto.randomUUID();
+  const url =
+    `https://www.figma.com/oauth?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&state=${state}&response_type=code`;
+  setCookie(c, "state", state);
+  return c.redirect(url);
 });
 
 app.get("/oauth/signout", (c: Context) => {
-  return signOut(c.req.raw);
 });
 
 app.get("/oauth/callback", async (c: Context) => {
-  console.log("Before callback");
-  console.log(c.req.raw);
-  const { response, tokens, sessionId } = await handleCallback(
-    c.req.raw,
-    oauthConfig,
-  );
-  console.log("After callback");
-  console.log(response);
-  return response;
+  const code = c.req.query("code");
+  const url =
+    `https://www.figma.com/api/oauth/token?client_id=${clientId}&client_secret=${clientSecret}&redirect_uri=${redirectUri}&code=${code}&grant_type=authorization_code`;
+  const response = await fetch(url, {
+    method: "POST",
+    mode: "cors",
+    redirect: "follow",
+  });
+  const tokens = await response.json();
+  // Token schema
+  // user_id
+  // access_token
+  // refresh_token
+  // expires_in
+  console.log(tokens);
+
+  return c.text(`${tokens.user_id}`);
 });
 
 app.get("/test/protected-route", async (c: Context) => {
-  return await getSessionId(c.req.raw) === undefined
-    ? new Response("Unauthorized", { status: 401 })
-    : new Response("You are allowed");
 });
 
 export default app;
