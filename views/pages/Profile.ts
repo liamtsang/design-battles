ba55975c329292df97d3c90903e4c7097c8ddcb1
Liam Tsang <lt1800@nyu.edu>
2024-02-28 19:55:06 -0500

    test commit for correct user

--- views/pages/Profile.ts
+++ views/pages/Profile.ts
@@ -1,5 +1,6 @@
 import { html } from "https://deno.land/x/hono@v4.0.4/helper.ts";
 import { User } from "../../utils/types.ts";
+
 export default function Profile(user: User) {
   return html`
     <div>${user.handle}</div>
