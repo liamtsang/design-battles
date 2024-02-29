ac9547cdfe5211744d2253d4ed6a3b32f20b9fa0
Liam Tsang <lt1800@nyu.edu>
2024-02-28 23:18:13 -0500

    added rank to profile page

--- views/pages/Profile.ts
+++ views/pages/Profile.ts
@@ -5,5 +5,9 @@ export default function Profile(user: User) {
   return html`
     <div>${user.handle}</div>
     <img src="${user.profile.avatarUrl}" />
+    <div id="rank">
+      <h1>Rank</h1>
+      <h4>${user.rank.rankTitle}</h4>
+    </div>
   `;
 }
